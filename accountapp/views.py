321b47b07fa6e21873fe884e34a014ba30dad73c
eka72 <dideka8809@gmail.com>
2021-08-30 09:48:59 +0900

    2021-08_30_git_study3

--- accountapp/views.py
+++ accountapp/views.py
@@ -17,23 +17,10 @@ from django.urls import reverse, reverse_lazy
 from articleapp.models import Article
 
 
-@login_required
-def hello_world(request):
-    if request.method == "POST":
-        temp = request.POST.get('hello_world_input')
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
-    else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
-
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     template_name = 'accountapp/create.html'
 
 
@@ -68,6 +55,6 @@ class AccountUpdateView(UpdateView):
 @method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
-    success_url = reverse_lazy('accountapp:hello_world')
+    success_url = reverse_lazy('articleapp:list')
     context_object_name = 'target_user'
     template_name = 'accountapp/delete.html'
